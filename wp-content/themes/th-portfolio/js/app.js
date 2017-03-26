0b397822c4f9bbc2a8337e14b5972f08e8a72561
Puyih Severino <yihseverino@gmail.com>
2017-03-26 14:19:27 +0800

    starting a wp theme

---
+++ wp-content/themes/th-portfolio/js/app.js
@@ -0,0 +1,19 @@
+$( ".nav-toggle" ).click(function() {
+  $(this).toggleClass("open");
+  $("nav").fadeToggle(100);
+
+  return false;
+});
+
+JQuery(document).ready( function($) {
+  
+  $(document).foundation();
+
+  $( ".nav-toggle" ).click(function() {
+    $(this).toggleClass("open");
+    $("nav").fadeToggle(100);
+
+    return false;
+  });
+}); 
+
